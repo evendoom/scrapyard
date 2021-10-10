6c919b0dc8d23049b929c235694aef6e1f55a760
evendoom <davidej.p.correia@gmail.com>
2021-10-10 16:44:24 +0000

    feat: Build login functionality

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from .signals import profile_send
 
@@ -43,5 +44,36 @@ def register_page(request):
 
 def login_page(request):
     """ Render login page """
+
+    if request.method == 'POST':
+
+        # Create dictionary from form elements
+        response = {
+            'username': request.POST.get('email'),
+            'password': request.POST.get('password')
+        }
+
+        # Check if username exists
+        try:
+            user = User.objects.get(username=response['username'])
+            print(user)
+        except:  # noqa: E722
+            # Add flash message here
+            print('Incorrect login details')
+
+        # Authenticate user
+        user = authenticate(
+            request,
+            username=response['username'],
+            password=response['password']
+        )
+
+        # Log user in
+        if user is not None:
+            login(request, user)
+            return redirect('main_page')
+        else:
+            # Add flash message
+            print('Incorrect Username / Password')
 
     return render(request, 'profiles/login.html')
